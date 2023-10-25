2d4c0c60f2f58cd669acbb08d3a524379e32ce48
CBowers28 <cj.bowers@ufl.edu>
2023-10-25 12:16:32 -0400

    decoder function complete.

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ def main():
         return new_password
     endcoded_password = encoder()
 
+#decoder function needs new_password passed into it
+    def decoder(new_password):
+        original_password = ""
+        for char in new_password:
+            #ittrates through each character in new_password and subtracts 3 then adds it to empty string original_password
+            new_password_character = int(char) - 3
+            original_password += str(new_password_character)
+            return print(f"The encoded password is {new_password}, and the original password is {original_password}.")
+
+
+
+
 
 if __name__ == "__main__":
     main()
